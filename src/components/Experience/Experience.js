62800e75182f8c622b3e728175702f6cca635d57
Prajneya <prajneya.kumar@research.iiit.ac.in>
2021-05-15 19:22:44 +0530

    add: sample experience and update about

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -11,7 +11,15 @@ class Experience extends Component {
 					<div className="subtopic-heading">been there. done that.</div>
 					<hr />
 					<div className="my-5 px-3">
-						
+						<div className="row">
+							<div className="col-lg-6">
+								<img src="" alt="exp"/>
+							</div>
+							<div className="col-lg-6">
+								<div className="card-header">sample exp</div>
+								<div className="m-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus erat massa, imperdiet sed fermentum ac, consectetur consequat quam.</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</>
