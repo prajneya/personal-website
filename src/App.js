064075020fc8c549383daa3b758b62d4b2ce8d73
Prajneya <prajneya.kumar@research.iiit.ac.in>
2021-05-15 18:46:10 +0530

    design: add animation bg and custom cursor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AnimatedCursor from "react-animated-cursor";
+import StarfieldAnimation from 'react-starfield-animation';
 
 import Home from './pages/Home/Home';
 import Work from './pages/Work/Work';
@@ -12,6 +14,21 @@ import Skills from './pages/Skills/Skills';
 function App() {
   return (
     <BrowserRouter>
+        <AnimatedCursor
+      innerSize={8}
+      outerSize={8}
+      color='102, 252, 241'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      />
+      <StarfieldAnimation
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%'
+        }}
+      />
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/work" component={Work} exact />
